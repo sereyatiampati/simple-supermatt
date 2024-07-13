9d527b7d84e757f9c8a095d6c33f20cc8f3cc0d3
Emilly Tiampati <sereyatiampatistudies@gmail.com>
2024-07-13 08:39:30 +0300

    learnt about the decorator and meta data that comes with it

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,6 +1,11 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-header'
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    standalone: true,
+    styleUrl: './header.component.css',
+    // styleUrls: [./header.component.css, ./another-stylingsheet.css] this takes an array of styling sheet that you want included
+    // styles: ['h1 { color: red}'] //this is in line styling
 })
 export class HeaderComponent {}
