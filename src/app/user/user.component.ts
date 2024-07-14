981c317a455a09ef8c19c2157a602847436aa4b1
Emilly Tiampati <sereyatiampatistudies@gmail.com>
2024-07-14 22:49:16 +0300

    added an event listener with Event Binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,4 +12,12 @@ const randomIndex=Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class UserComponent {
   selectedUser=DUMMY_USERS[randomIndex]
+
+  get imagePath() {
+    return '/users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser() {
+    console.log("Clicked!");
+  }
 }
