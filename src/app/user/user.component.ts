f59d6f400d3b5fbd13b32785e4ca0f29902dac03
Emilly Tiampati <sereyatiampatistudies@gmail.com>
2024-07-15 23:19:07 +0300

    using signal inputs and property binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +8,15 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-    @Input() avatar: string;
+    // @Input({required: true}) avatar!: string; //the exclamation simply tells typescript that we know that this will definetely be to some value even if though typescript can't see it from this code
+    // @Input({required:true}) name!: string;
 
+    avatar = input.required<string>(); //to tell angular that the value will be set outside this component and it will be required we use: avarar = input.required<string>();
+    name = input.required<string>();
+
+
+    get imagePath() {
+      return '/users/' + this.avatar
+    }
   onSelectUser() {}
 }
